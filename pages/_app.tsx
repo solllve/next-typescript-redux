98fd7a7b7941809fd5beee67c0ed76240c071870
solllve <caseyhald@Caseys-MacBook-Pro.local>
2021-12-12 15:02:47 -0800

    ah crud

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,60 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
-import store from '../store/store'
+import {createStore} from 'redux';
+
+
+export const bugAdded = description => ({
+  type: 'bugAdded',
+  payload: {
+      description
+  }
+})
+
+export const bugRemoved = id => ({
+  type: 'bugRemoved',
+  payload: {
+      //if key and value are same then we can use shorthand syntax
+      id
+  }
+})
+
+export const bugResolved = id => ({
+  type: 'bugResolved',
+  payload: {
+      //if key and value are same then we can use shorthand syntax
+      id
+  }
+})
+
+let lastId = 0
+
+function reducer(state = [], action){
+  switch(action.type){
+      case 'bugAdded':
+          return [
+              ...state,
+              {
+                  id: ++lastId,
+                  description: action.payload.description,
+                  resolved: false
+              }
+          ];
+      case 'bugRemoved':
+          return state.filter(bug => bug.id !== action.payload.id);
+      case 'bugResolved':
+          //update the bug with the id with updated resolved to true
+          return state.map(bug => bug.id === action.payload.id ? {...bug, resolved: true} : bug);
+      default:
+          return state;
+  }
+}
+
+const store = createStore(reducer);
+
+
 function MyApp({ Component, pageProps }: AppProps) {
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,32 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { bugAdded, bugResolved, bugRemoved } from './_app'
+import { useDispatch, useSelector } from "react-redux";
+import React, {useEffect} from 'react';
 
 const Home: NextPage = () => {
+  const dispatch = useDispatch();
+  const store = useSelector(state => state);
+
+  useEffect(() => {
+    // show me the updates
+    console.log(store);
+  });
+
   return (
     <div className={styles.container}>
       <h1>Next Typescript Redux</h1>
-      
+      {store.map(bug => (
+        <div key={bug.id}>
+          <h2>{bug.title}</h2>
+          <p>{bug.description}</p>
+          <p>{bug.status}</p>
+          <button onClick={() => dispatch(bugResolved(bug.id))}>Resolve</button>
+          <button onClick={() => dispatch(bugRemoved(bug.id))}>Remove</button>
+        </div>
+      ))}
+      <button onClick={() => dispatch(bugAdded('This is the description'))}>Add Bug</button>
     </div>
   )
 }
